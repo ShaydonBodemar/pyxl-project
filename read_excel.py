2417ed4a7b1f74500dd212db2fdc4ee18ef3d1f3
ShaydonBodemar <shaydon.bodemar@gmail.com>
2020-09-22 01:24:23 -0700

    Added module comments and simplified constructor

--- read_excel.py
+++ read_excel.py
@@ -6,28 +6,52 @@
 from openpyxl import load_workbook
 
 class ReadConfig:
+    """
+    @brief Constructor for this module
+    @param excel_filename Used to determine the file that will be interacted with.
+    @note Only opens the file to be used and initializes the member variables.
+    """
     def __init__(self, excel_filename):
-        workbook = load_workbook(excel_filename, read_only=True)
-        chip_profiles = workbook['Chip Roadmap']
+        self._workbook = load_workbook(excel_filename, read_only=True)
+        self._metadata_fields = []
+        self._chip_profiles = {}
+    
+
+    """
+    @brief Destructor for this module
+    @note Closes workbook used for reading sheets in this module.
+    """
+    def __del__(self):
+        self._workbook.close()
+
+
+    """
+    @brief Reads the metadata for each chip (scaler, TO date, etc)
+    @param worksheet_name Name of the worksheet to be used for reading in appropriate data
+    @note Wipes any metadata currently stored
+    """
+    def ReadChipMetaData(self, worksheet_name):
+        chip_profiles = self._workbook[worksheet_name]
 
         # Reads in the data labels for the first row to be used later as keys in dict
-        self.metadata_fields = []
+        self._metadata_fields = []
         for cell in chip_profiles[1]:
-            self.metadata_fields.append(cell.value)
+            self._metadata_fields.append(cell.value)
 
         # Reads in all data fields to create a full nested dictionary representing each chip's metadata
-        self.chip_profiles = {}
+        self._chip_profiles = {}
         for row in chip_profiles.iter_rows(min_row=2):
             chip_name = row[0].value
-            self.chip_profiles[chip_name] = {}
+            self._chip_profiles[chip_name] = {}
             for cell in range(1,len(row)):
-                self.chip_profiles[chip_name][self.metadata_fields[cell]] = row[cell].value
+                self._chip_profiles[chip_name][self._metadata_fields[cell]] = row[cell].value
+        # print(self._chip_profiles)
 
 
-        workbook.close()
-
-    def ReadChipMetaData:
-        # TODO: reading of the metadata for each chip
-
-    def ReadChipTierData:
+    """
+    @brief Reads the tool usage profiles for each chip tier
+    @param worksheet_name Name of the worksheet to be used for reading in appropriate data
+    """
+    def ReadChipTierData(self, worksheet_name):
         # TODO: read in TO-based data for every tier of chips
+        print(1)
